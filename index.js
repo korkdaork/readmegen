0224e2a6a2642e375b1f12f29a1249533c980e26
Yev <62456182+korkdaork@users.noreply.github.com>
2020-06-04 22:20:08 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
-const util = requirre("util");
+const util = require("util");
 
 const writeFileAsync = util.promisify(fs.writeFile);
 
